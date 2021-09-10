5fc80efa2bc249136795beac06fc785b3573dac8
Timothy Dykes <timothywanedykes@gmail.com>
2021-09-09 17:52:52 -0700

    change readFile to sync

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -10,13 +13,8 @@ const server = http.createServer((req, res) => {
   } else if(pathName === '/product') {
     res.end('This is the PRODUCT');
   } else if( pathName === '/api') {
-    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { 'Content-type': 'application/json' });
-      //console.log(productData);
-      res.end(data);
-    });
-
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html'
