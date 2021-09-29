c3fee661e543d5e099f452b99ed2e774ab6b6585
Timothy Dykes <timothywanedykes@gmail.com>
2021-09-28 18:19:05 -0700

    Add function to replace individual cards and card sections

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const http = require('http');
 const url = require('url');
 
 ///// Server
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if(!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+
+}
+
 const tempOverview = fs.readFileSync(`${__dirname}/template/overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/template/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/template/product.html`, 'utf-8');
@@ -17,9 +34,11 @@ const server = http.createServer((req, res) => {
   if(pathName === '/' || pathName === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    //console.log(cardsHtml);
 
-    res.end(tempOverview);
+    res.end(output);
 
   // Product page
   } else if(pathName === '/product') {
