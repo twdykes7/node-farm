4930fcbd1b0e9801ad7c93713fdb49dc8f9cf4a7
Timothy Dykes <timothywanedykes@gmail.com>
2021-09-29 16:50:28 -0700

    require exported module replaceTemplate, add path for /product

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+//core modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+//own modules
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ///// Server
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
+// const replaceTemplate = (temp, product) => {
+//   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+//   output = output.replace(/{%IMAGE%}/g, product.image);
+//   output = output.replace(/{%PRICE%}/g, product.price);
+//   output = output.replace(/{%FROM%}/g, product.from);
+//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//   output = output.replace(/{%ID%}/g, product.id);
 
-  if(!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-  return output;
+//   if(!product.organic) {
+//     output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//   }
+//   return output;
 
-}
+// }
 
 const tempOverview = fs.readFileSync(`${__dirname}/template/overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/template/template-card.html`, 'utf-8');
@@ -28,10 +32,13 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+
+  // console.log(req.url);
+  const { query, pathname } = (url.parse(req.url, true));
+  // const pathname = req.url;
 
   // Overview page
-  if(pathName === '/' || pathName === '/overview') {
+  if(pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -41,11 +48,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
   // Product page
-  } else if(pathName === '/product') {
-    res.end('This is the PRODUCT');
+  } else if(pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
 
   // API
-  } else if( pathName === '/api') {
+  } else if( pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
